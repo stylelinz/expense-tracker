b27f7ca6bb8463e0c8f829ec5ed311621519e33b
stylelinz <cwchuang1997@gmail.com>
2021-05-11 09:09:48 +0800

    feat: project init + app.js #20210511001

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express')
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('<h1>Expense Tracker!!</h1>')
+})
+
+app.listen(port, () => console.log(`Express is listening on http://localhost:${port}`))

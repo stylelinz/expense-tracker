05079a0502a623f5b7f82dc489571ad8db3e2791
stylelinz <cwchuang1997@gmail.com>
2021-05-12 14:22:13 +0800

    feat: add more details at reading expenses function + home.js, index.hbs #20210512002

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -2,10 +2,13 @@ const express = require('express')
 const router = express.Router()
 
 const Record = require('../../models/Record')
-// const Category = require('../../models/Category')
+const Category = require('../../models/Category')
 
 router.get('/', async (req, res) => {
-  const records = await Record.find().lean()
+  // Render Category options
+  const records = await Record.find().populate({
+    path: 'categoryId'
+  }).lean()
   return res.render('index', { records })
 })
 

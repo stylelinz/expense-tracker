1aaf49639968fe496aa139716bec33be5d6c3d0d
stylelinz <cwchuang1997@gmail.com>
2021-05-12 21:34:11 +0800

    feat: add total amount function + home.js, index.hbs #20210512008

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -9,7 +9,8 @@ router.get('/', async (req, res) => {
   const records = await Record.find().populate({
     path: 'categoryId'
   }).lean()
-  return res.render('index', { records })
+  const totalAmount = records.map(record => record.amount).reduce((prev, current) => prev + current)
+  return res.render('index', { records, totalAmount })
 })
 
 module.exports = router

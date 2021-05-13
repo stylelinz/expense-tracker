affe925206f5a65c7612199758cd80a77b01b301
stylelinz <cwchuang1997@gmail.com>
2021-05-13 09:26:09 +0800

    feat: add category filter function + home,js, index.hbs #20210513001

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -5,12 +5,21 @@ const Record = require('../../models/Record')
 const Category = require('../../models/Category')
 
 router.get('/', async (req, res) => {
-  // Render Category options
-  const records = await Record.find().populate({
-    path: 'categoryId'
-  }).lean()
-  const totalAmount = records.map(record => record.amount).reduce((prev, current) => prev + current)
-  return res.render('index', { records, totalAmount })
+  try {
+    const { category } = req.query
+    const categories = await Category.find().lean()
+    // Render Category options
+    let records = await Record.find().populate({ path: 'categoryId' }).lean().sort({ date: 'desc' })
+    const filteredRecords = records.filter(record => record.categoryId.categoryName === category)
+    if (category) {
+      records = filteredRecords
+    }
+    const amounts = records.map(record => record.amount)
+    const totalAmount = amounts.length ? amounts.reduce((prev, current) => prev + current) : 0
+    return res.render('index', { records, totalAmount, categories })
+  } catch (err) {
+    console.log(err)
+  }
 })
 
 module.exports = router

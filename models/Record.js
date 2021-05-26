64eaf06f8e502c8328ecb90b8a642deaed2a1b26
stylelinz <cwchuang1997@gmail.com>
2021-05-26 15:29:34 +0800

    refactor: modify records' schema & seeder #20210526001

--- models/Record.js
+++ models/Record.js
@@ -5,7 +5,7 @@ const { Schema } = mongoose
 const recordSchema = new Schema({
   name: { type: String, required: true },
   date: { type: Date, default: Date.now, required: true },
-  categoryId: { type: Schema.Types.ObjectId, ref: 'Categories', required: true },
+  category: { type: String, required: true },
   amount: { type: Number, required: true }
 })
 
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,5 +1,4 @@
 const Record = require('../Record')
-const Category = require('../Category')
 const db = require('../../config/mongoose')
 
 const records = require('./records.json')
@@ -7,12 +6,7 @@ const records = require('./records.json')
 db.once('open', async () => {
   try {
     for (const record of records) {
-      const category = await Category.findOne({ categoryName: record.category }).lean()
-      await Record.create({
-        name: record.name,
-        amount: record.amount,
-        categoryId: category._id
-      })
+      await Record.create(record)
       console.log(`Seed: ${record.name} created.`)
     }
     await db.close()

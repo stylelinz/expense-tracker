b6181ac4b803e0a490c024dccd263e86d21fcb2f
stylelinz <cwchuang1997@gmail.com>
2021-05-27 22:19:44 +0800

    feat: add create, edit form validation (HTML) + public/js/formValidate.js #20210527004

---
+++ public/js/formValidate.js
@@ -0,0 +1,9 @@
+const form = document.querySelector('.needs-validation')
+
+form.addEventListener('submit', (event) => {
+  if (!form.checkValidity()) {
+    event.preventDefault()
+    event.stopPropagation()
+  }
+  form.classList.add('was-validated')
+}, false)

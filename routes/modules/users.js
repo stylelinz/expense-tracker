f3e39264dca59baaa80940533be15871068601ea
stylelinz <cwchuang1997@gmail.com>
2021-05-29 15:10:55 +0800

    feat: add bcrypt at register #20210529005

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,5 +1,8 @@
 const router = require('express').Router()
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
+
+const User = require('../../models/User')
 
 router.get('/login', (req, res) => {
   return res.render('login')
@@ -14,7 +17,18 @@ router.post('/login', passport.authenticate('local', {
   failureRedirect: '/users/login'
 }))
 
-router.post('/register')
+router.post('/register', async (req, res) => {
+  try {
+    const { name, email, password } = req.body
+    // 為輸入的密碼加入鹽與雜湊
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(password, salt)
+    await User.create({ name, email, password: hash })
+    return res.redirect('/')
+  } catch (err) {
+    console.log(err)
+  }
+})
 
 router.get('/logout', (req, res) => {
   req.logout()

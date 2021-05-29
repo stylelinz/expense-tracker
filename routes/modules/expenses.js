c5d738fb6d28007eae601698778f8a517b17e638
stylelinz <cwchuang1997@gmail.com>
2021-05-29 17:48:52 +0800

    feat: add express-validator + middleware/validate.js #20210529006

--- routes/modules/expenses.js
+++ routes/modules/expenses.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const router = express.Router()
+const { validateResult } = require('express-validator')
 
 const Record = require('../../models/Record')
 const Category = require('../../models/Category')
+const validator = require('../../middleware/validate')
 // dateFormat函式會將輸入的日期格式轉換為瀏覽器支援的'YYYY-MM-DD'格式，
 // 沒有輸入值時，預設為今天日期
 const dateFormat = require('../../util/formattedDate')
 
-router.get('/create', async (req, res) => {
+router.get('/create', validator.record, async (req, res) => {
   const categories = await Category.find().lean()
   const today = dateFormat()
   return res.render('create', { categories, today })

4236af1fcfd8668279a77f6b45325fb555bbb123
stylelinz <cwchuang1997@gmail.com>
2021-05-30 21:13:46 +0800

    feat: modify seeders #20210530009

--- models/seeds/categorySeeder.js
+++ models/seeds/categorySeeder.js
@@ -1,3 +1,6 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const Category = require('../Category')
 const db = require('../../config/mongoose')
 const categories = require('./categories.json')
@@ -7,8 +10,9 @@ db.once('open', async () => {
     for (const category of categories) {
       await Category.create(category)
     }
+    console.log('Category seeder finished.')
     await db.close()
-    console.log('Close database...')
+    console.log('Close database.')
   } catch (err) {
     console.log(err)
   }
--- models/seeds/recordSeeder.js
+++ models/seeds/recordSeeder.js
@@ -1,16 +1,36 @@
-const Record = require('../Record')
+const bcrypt = require('bcryptjs')
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const db = require('../../config/mongoose')
 
+const Record = require('../Record')
+const User = require('../User')
 const records = require('./records.json')
+
+const SEED_USER = {
+  name: 'user1',
+  email: 'user1@example.com',
+  password: '12345678'
+}
 
 db.once('open', async () => {
   try {
+    const salt = await bcrypt.genSalt(10)
+    const hash = await bcrypt.hash(SEED_USER.password, salt)
+    const user = await User.create({
+      name: SEED_USER.name,
+      email: SEED_USER.email,
+      password: hash
+    })
+    const { _id: userId } = user
     for (const record of records) {
-      await Record.create(record)
+      await Record.create(Object.assign(record, { userId }))
       console.log(`Seed: ${record.name} created.`)
     }
+    console.log('All seeds are done.')
     await db.close()
-    console.log('Close database...')
+    console.log('Close database.')
   } catch (err) {
     console.log(err)
   }
